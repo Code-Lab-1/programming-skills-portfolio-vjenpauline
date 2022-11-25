e89a74697836e8959f65104afc0a62e297284015
vjenpauline <113340493+vjenpauline@users.noreply.github.com>
2022-11-25 12:41:01 +0400

    Chapter 5 Updates

--- Chapter 5- Dictionaries/Exercises/03_Glossary2.py
+++ Chapter 5- Dictionaries/Exercises/03_Glossary2.py
@@ -5,12 +5,14 @@ glossary = {
     'Float':'stores a decimal number', 
     'Function':'a reusable unit of code assigned to a fixed task', 
     'Index':'addresses elements of an ordered series', 
-    'Docstring':'a string literal to explain functions, classes, etc.',
+    'Docstring':'a string literal to explain functions, classes, etc.'}
+
+glossary.update({
     'Concanetation':'merging strings',
     'Algorithm':'steps to perform a task',
     'len() Function':'used to find length of an object',
     'print() Function':'used to print a message',
-    'range() Function':'used to show a sequence of numbers' } # Dictionary of Python terms with 5 more additions
+    'range() Function':'used to show a sequence of numbers'})
 
 for key, value in glossary.items(): 
     print(f"{key} \n- {value} \n") # Outputs the word name(key) and its meaning(value) in an organized format with a loop
