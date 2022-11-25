370c8ca53ea14661be53672be2324f6b54fad130
vjenpauline <113340493+vjenpauline@users.noreply.github.com>
2022-11-25 13:14:57 +0400

    Chapter 6 Update Exercises

--- Chapter 6- Loops/Exercises/08_Asking Dessert.py
+++ Chapter 6- Loops/Exercises/08_Asking Dessert.py
@@ -1,11 +1,11 @@
 # Chapter 6, Exercise 8: Asking Dessert
 
 favs = []
-dessert = str(input('What\'s your favorite dessert?: '))
+dessert = str(input('Do you like desserts?: '))
 
 # Asks for the user's favorite desserts and adds them to the list of favs
 while dessert != "quit":
-    dessert = str(input('What are more of your favorite dessert?: '))
+    dessert = str(input('What are your favorite desserts?: '))
     if dessert != "quit":
         favs.append(dessert)
 
